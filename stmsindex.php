f7dd1bd04e0ca904f5a651b6779cb19c5b3ab1c4
kelvin David Kimani <35039377+k3lv1nd@users.noreply.github.com>
2018-01-20 20:14:39 +0300

    Add files via upload

---
+++ stmsindex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<link rel="stylesheet" href="stmsstyles.css">
+	<h3>Welcome to the Simple Time Management System</h3>
+</head>
+
+<p>Please enter your details below to create an account. If you alredy have an account click<a href=stmslogin.php>here</a>to Log in into your account</p>
+
+		<form name="register" method="POST" action="?">
+			<p>Forename  <input type="text" name="forename" maxlength="30" size="30"></p>
+			<p>Surname   <input type="text" name="surname" maxlength="30" size="30"></p>
+			<p>Email     <input type="text" name="email" maxlength="30" size="30"></p>
+			<p>Password  <input type="Password" name="Password" maxlength="30" size="30"></p>
+			<input type="submit" name="submit" value="submit">
+
+		</form>
