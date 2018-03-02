24ca75e73c74b058335e012fda6adef07fee2e9a
kelvin David Kimani <35039377+k3lv1nd@users.noreply.github.com>
2018-03-02 11:29:00 +0300

    Update stmslogin.php

--- Registration&login/stmslogin.php
+++ Registration&login/stmslogin.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
+print"welcome user, please enter your email and password to login<br />";
+
+
 ?>
 <!DOCTYPE html>
+
 <html>
 <head>
+	<link rel="stylesheet" href="stmsstyles.css">
 	<title>login user</title>
 	<form name="login" method=post action="stmsmembers.php">
 	Input email<input type="text" name="email" maxlength="30"><br />
